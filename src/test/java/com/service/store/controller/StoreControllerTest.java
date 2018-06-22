059d2049ce124ae13441373f48aa69d577be91cc
Bruno Filho <brgccf@cin.ufpe.br>
2018-06-22 16:01:10 -0300

    finishing tests but minor bug to resolve

--- src/test/java/com/service/store/controller/StoreControllerTest.java
+++ src/test/java/com/service/store/controller/StoreControllerTest.java
@@ -34,33 +34,105 @@ public class StoreControllerTest {
 	@Autowired
 	private WebApplicationContext context;
 	
+	
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	private MockMvc mockMvc;
+	
+	public String createStoreAndReturnToken() {
+		String newStoreUrl = "/store";
+		String token = "";
+		ArrayList<Level> levels = new ArrayList<Level>();
+		
+		for(int i = 0; i < 3; i++) {
+			levels.add(new Level(1000.0, 5.0-i, 10.0+i));
+		}
+		
+		Gson gson = new Gson();
+		String jsonInString = gson.toJson(levels);
+		
+		
+		try {
+			this.mockMvc.perform(post(newStoreUrl)
+					.header("username", "dstore").header("password", "opa13")
+					.header("oldDaysPurchases", 100.0)
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(jsonInString))
+					.andDo(print())
+					.andExpect(status().is2xxSuccessful())
+					.andReturn();
+			
+			//created store, now login to get token
+			MvcResult result = this.mockMvc.perform(get("/login").header("username", "dstore")
+					.header("password", "opa13")).andExpect(status().isOk())
+					.andDo(print()).andReturn();
+			
+			token = result.getResponse().getContentAsString();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		return token;
+		
+		
+	}
+	
+	public void deleteStore(String token) throws Exception {
+		this.mockMvc.perform(delete("/store").header("token", token))
+			.andExpect(status().isOk());
+	}
 	
 	@Before
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
 	}
 	
+	/**
+	 * test for login on store preloaded & change levels
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testStore() throws Exception {
-		String getLoginUrl = new StringBuilder()
-				.append("/login").toString();
+			
 		
-		
-		MvcResult result = this.mockMvc.perform(get(getLoginUrl).header("username", "laroca")
-				.header("password", "larocastore")).andExpect(status().isOk())
-				.andDo(print()).andReturn();
-		
-		
-		String content = result.getResponse().getContentAsString();
-		testLevels(content);
+		String token = createStoreAndReturnToken();
+		testLevels(token);
 	}
 	
 	public void testLevels(String token) throws Exception {
 		String getLevelUrl = new StringBuilder()
 				.append("/levels").toString();
+		
+		ArrayList<Level> levels = new ArrayList<Level>();
+		
+		for(int i = 0; i < 3; i++) {
+			levels.add(new Level(5700.0, 7.0-i, 14.0+i));
+		}
+		
+		Gson gson = new Gson();
+		String jsonInString = gson.toJson(levels);
+		
+		
+		this.mockMvc.perform(put(getLevelUrl).header("token", token)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonInString))
+				.andDo(print())
+				.andExpect(status().is4xxClientError())
+				.andReturn();
+		
+		deleteStore(token);
+		
+	}
+	
+	/**
+	 * test creation of new stores.
+	 * needs username, password, oldDaysPurchases (double) and levels
+	 * @throws Exception 
+	 */
+	@Test
+	public void testCreateStore() throws Exception {
+		String newStoreUrl = "/store";
 		
 		ArrayList<Level> levels = new ArrayList<Level>();
 		
@@ -72,14 +144,38 @@ public class StoreControllerTest {
 		String jsonInString = gson.toJson(levels);
 		
 		
-		this.mockMvc.perform(put(getLevelUrl).header("token", token)
+		this.mockMvc.perform(post(newStoreUrl)
+				.header("username", "pqnstore").header("password", "porquin")
+				.header("oldDaysPurchases", 100.0)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(jsonInString))
 				.andDo(print())
-				.andExpect(status().isOk())
+				.andExpect(status().is2xxSuccessful())
 				.andReturn();
 		
+		//login to delete store
+		MvcResult result = this.mockMvc.perform(get("/login").header("username", "pqnstore")
+				.header("password", "porquin")).andExpect(status().isOk())
+				.andDo(print()).andReturn();
 		
-				
+		String token = result.getResponse().getContentAsString();
+		
+		deleteStore(token);
 	}
+	
+	/**
+	 * testing info for registers (purchases in store)
+	 * @throws Exception 
+	 * 
+	 */
+	@Test
+	public void testRegisterInfo() throws Exception {
+		String token = createStoreAndReturnToken();
+		
+		this.mockMvc.perform(get("/registerinfo").header("token", token))
+			.andExpect(status().isOk()).andReturn();
+		
+		deleteStore(token);
+	}
+	
 }
